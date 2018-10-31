20e07691b718d1736ef6f408754c5743829ec729
Maharshee roy <m.roy@optonline.net>
2018-10-31 17:36:40 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@ def speak_to_grandma(string)
   if phrase === phrase.Upper
     puts "NO, NOT SINCE 1938!"
   elsif phrase === "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!" 
+    return "I LOVE YOU TOO PUMPKIN!" 
   else 
     puts "HUH?! SPEAK UP SONNY!"
 end
